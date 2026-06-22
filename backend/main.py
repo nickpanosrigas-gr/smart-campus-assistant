3ef734445e224da80f7316c3349d0e82ce7937ac
nickpanosrigas-gr <nickpanosrigas@gmail.com>
2026-06-23 01:46:30 +0300

    Finalized the Floor design

--- backend/main.py
+++ backend/main.py
@@ -69,4 +69,4 @@ async def websocket_endpoint(websocket: WebSocket):
         logger.error(f"[WEBSOCKET] Error: {e}")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
